a6c2387f7da75fe84c7db540822b13b42795358f
artem_radiuk <61933559+gra4ina@users.noreply.github.com>
2022-07-05 18:56:49 +0300

    H/W #6 Birds Assignment

--- src/Zoo.java
+++ src/Zoo.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Zoo {
@@ -7,10 +8,11 @@ public class Zoo {
    *
    * @param list list of birds of particular type.
    */
-  public static void feed(List list) {
+  public static void feed(List<? extends Bird> list) {
     // TODO fix method declaration
     System.out.println("Feeding birds");
     // TODO feed birds in the cycle
+    for (Bird bird : list) bird.eat();
   }
 
   /**
@@ -19,11 +21,14 @@ public class Zoo {
    * @param list list of birds of particular type.
    * @param bird a new bird.
    */
-  public static void acceptBird(List list, FlyingBird bird) {
+  public static void acceptBird(List <? super FlyingBird> list, FlyingBird bird) {
     // TODO fix method declaration
+
     System.out.println("Accepting a bird to a section");
     // TODO check flying bird wings
     // TODO add a bird to the list and print it
+    bird.checkWings();
+    list.add(bird);
   }
 
   /**
@@ -32,10 +37,12 @@ public class Zoo {
    * @param list    list of birds.
    * @param newBird bird to add.
    */
-  public static void registerBird(List list, Bird newBird) {
+  public static void registerBird(List <Bird> list, Bird newBird) {
     // TODO fix method declaration
     System.out.println("Adding a bird to the birds list");
-    // TODO add a bird to list
-    // TODO print birds in the cycle
+    list.add(newBird);
+    for (Object bird : list) {
+      System.out.println(bird);
+    }
   }
 }
